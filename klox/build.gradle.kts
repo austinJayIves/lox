95959a7c80786af540515931c1c5f73d24342588
Jay Ives <jay@Osiris.home>
2022-12-01 23:30:05 -0800

    Initial commit

---
+++ klox/build.gradle.kts
@@ -0,0 +1,30 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("jvm") version "1.7.20"
+    application
+}
+
+group = "me.jay"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions.jvmTarget = "14"
+}
+
+application {
+    mainClass.set("MainKt")
+}
